afcb16de279a563ca3ee72a0caaff17c658a00ff
Matthew Skelton <contact@matthewskelton.net>
2014-06-11 23:04:17 +0100

    Removed spurious exclude file reference

--- scripts/LeanCopy.cmd
+++ scripts/LeanCopy.cmd
@@ -1,1 +1,1 @@
-xcopy /d /i /e /y /EXCLUDE:Excludes.txt %LEANPUB_GIT_DIR%\manuscript %DROPBOX_DIR%\%LEANPUB_BOOK_NAME%\manuscript
+xcopy /d /i /e /y %LEANPUB_GIT_DIR%\manuscript %DROPBOX_DIR%\%LEANPUB_BOOK_NAME%\manuscript
