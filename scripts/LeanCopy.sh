d454431d1362cc4c206c100e9547f5a8cad68d0e
Rob Thatcher <rob.t@mac.com>
2014-06-13 10:15:59 +0100

    Fixed case syntax

--- scripts/LeanCopy.sh
+++ scripts/LeanCopy.sh
@@ -3,10 +3,10 @@ case $OS in
   Darwin)
 	# MacOSX 10.9.3 (at least) has deprecated the -r option to cp, preserved for compatibility - RT 13/06/2014
 	# So we use rsync instead
-	rsync -auv $LEANPUB_GIT_DIR/manuscript $DROPBOX_DIR/$LEANPUB_BOOK_NAME/
+	rsync -auv $LEANPUB_GIT_DIR/manuscript $DROPBOX_DIR/$LEANPUB_BOOK_NAME/ ;;
   Linux)
 	# Linux cp, i.e. from GNU Coreutils 
-	cp -pru $LEANPUB_GIT_DIR/manuscript/ $DROPBOX_DIR/$LEANPUB_BOOK_NAME/
+	cp -pru $LEANPUB_GIT_DIR/manuscript/ $DROPBOX_DIR/$LEANPUB_BOOK_NAME/ ;;
   *)
 	echo -e "\nUnable to determine *nix variant, so we bailed out.\n"
 	exit 1
