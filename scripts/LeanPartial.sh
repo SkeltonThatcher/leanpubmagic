b21a56caaba4e8fc39844c10c5b7bc214ba11b35
Rob Thatcher <rob@robthatcher.net>
2016-06-15 12:20:27 +0100

    Added a check for Subset.txt for partial subset preview to LeanPartial.sh

--- scripts/LeanPartial.sh
+++ scripts/LeanPartial.sh
@@ -1,1 +1,7 @@
+if [ ! -f "$LEANPUB_GIT_DIR/manuscript/Subset.txt" ];
+	then
+		echo -e "\nThe required 'Subset.txt' file is missing from the manuscript, cannot create partial sample, exiting.\n"
+		exit 1
+fi
+
 curl -d "api_key=$LEANPUB_API_KEY" https://leanpub.com/$LEANPUB_BOOK_NAME/preview/subset.json 
